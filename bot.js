63709826cda03dbaaa756a90d85d08236b36f5a8
gmbotleri <44378580+gmbotleri@users.noreply.github.com>
2018-10-22 21:50:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/test$/;
+      botRegex = /^\eric$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = "roger roger";
+  botResponse = "God damn";
 
   options = {
     hostname: 'api.groupme.com',
