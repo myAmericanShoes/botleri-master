53c26ae3d4e3f04837c02407b9fd745670a57b53
gmbotleri <44378580+gmbotleri@users.noreply.github.com>
2018-10-22 21:47:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = "roger roger";
 
   options = {
     hostname: 'api.groupme.com',
