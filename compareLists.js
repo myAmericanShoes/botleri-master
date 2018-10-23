e157f52e6b57ca4d062627bcc1ac793e79ec95c1
gmbotleri <44378580+gmbotleri@users.noreply.github.com>
2018-10-23 22:29:42 +0200

    Create compareLists.js

---
+++ compareLists.js
@@ -0,0 +1,5 @@
+const colorCode = {
+  captureCompare: ["eric", "Eric", "god Damn", "God damn"],
+  responseGrid: ["God damn"] //to be filled with more complicated replies at a later time
+};
+export { colorCode };
