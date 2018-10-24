77c77f88982ea293ab7aaf145345d7222c66fd34
gmbotleri <44378580+gmbotleri@users.noreply.github.com>
2018-10-24 23:55:48 +0200

    Update compareLists.js

--- compareLists.js
+++ compareLists.js
@@ -1,7 +1,19 @@
+
+
+
+
+
+//REDACTED, DON'T USE THIS FOR NOW
+
+
+
+
+
+
 /*const compareLists = {
   captureCompare: ['reg'],
   responseGrid: ["God damn"] //to be filled with more complicated replies at a later time
 };*/
 
 
-export const captureCompare = [/^\reg$/];
+//export const captureCompare = [/^\reg$/];
