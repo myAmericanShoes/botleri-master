9eddf2a9304493b5c76964fcae969abb6adb656b
gmbotleri <44378580+gmbotleri@users.noreply.github.com>
2018-10-24 21:47:09 +0200

    Update compareLists.js

--- compareLists.js
+++ compareLists.js
@@ -1,5 +1,5 @@
 const compareLists = {
-  captureCompare: ["eric", "Eric", "god Damn", "God damn"],
+  captureCompare: ["eric", "Eric", "overlord", "Overlord", "/reg"],
   responseGrid: ["God damn"] //to be filled with more complicated replies at a later time
 };
 export { compareLists };
